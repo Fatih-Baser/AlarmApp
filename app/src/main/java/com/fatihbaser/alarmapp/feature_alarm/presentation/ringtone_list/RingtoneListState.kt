337ab6f41256b8244055246cb53081a044b260e8
Fatih Baser <basese_aehit@students.vizja.pl>
2025-02-09 19:05:35 +0100

    RingtoneListScreen added

---
+++ app/src/main/java/com/fatihbaser/alarmapp/feature_alarm/presentation/ringtone_list/RingtoneListState.kt
@@ -0,0 +1,9 @@
+package com.fatihbaser.alarmapp.feature_alarm.presentation.ringtone_list
+
+import com.fatihbaser.alarmapp.core.domain.ringtone.NameAndUri
+
+
+data class RingtoneListState(
+    val ringtones: List<NameAndUri> = emptyList(),
+    val selectedRingtone: NameAndUri? = null
+)
